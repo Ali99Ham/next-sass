ac283adc7c3a19bfe4945834cf780fd985b7752d
unknown <ali.hamdan@wonderdays.com>
2025-03-21 22:39:43 +0400

    add table functions

--- src/app/dashboard/category/[name]/empty-category-state.tsx
+++ src/app/dashboard/category/[name]/empty-category-state.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 ////ADDing discord token /////
+///add table functions//
 export const EmptyCategoryState = ({
   categoryName,
 }: {
